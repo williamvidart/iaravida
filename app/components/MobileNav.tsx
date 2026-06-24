75d92767e6bf5b1673a3b7b3806e17e5233284dd
William Vidart <william.mb@hotmail.com>
2026-06-24 16:46:04 -0300

    Change Inicio nav link to Quem Sou section

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 const links = [
-  { href: "#topo", label: "Início" },
+  { href: "/#quem-sou", label: "Quem Sou" },
   { href: "/curso", label: "Curso Online" },
   { href: "/oraculo", label: "Oráculo Alma Criativa" },
   { href: "/artes-personalizadas", label: "Artes Personalizadas" },
